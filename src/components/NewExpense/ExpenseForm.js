8d3ea874322c4fd43ad8ddfe4f0faeb70a405e44
John Brady <jbradysd@icloud.com>
2022-08-01 14:33:25 -0700

    working with multiple states using state-slices approach or the one-state approach

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './ExpenseForm.css'
 
 const ExpenseForm = () => {
+  // Using 'State Slices' technique
+  const [enteredTitle, setEnteredTitle] = useState('')
+  const [enteredAmount, setEnteredAmount] = useState('')
+  const [enteredDate, setEnteredDate] = useState('')
+
+  // Using one state technique
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: '',
+  //     enteredAmount: '',
+  //     enteredDate: '',
+  //   })
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value)
+    // Part of the 'State Slices' technique
+    setEnteredTitle(event.target.value)
+
+    // Using one state technique
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // })
+
+    // Better approach to using one state
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredTitle: event.target.value }
+    // })
+  }
+
+  const amountChangeHandler = (event) => {
+    // Part of the 'State Slices' technique
+    setEnteredAmount(event.target.value)
+
+    // Using one state technique
+    // setUserInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // })
+
+    // Better approach to using one state
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredAmount: event.target.value }
+    // })
+  }
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value)
+
+    // Using one state technique
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // })
+
+    // Better approach to using one state
+    // setUserInput((prevState) => {
+    //   return { ...prevState, enteredDate: event.target.value }
+    // })
   }
 
   return (
@@ -16,11 +71,21 @@ const ExpenseForm = () => {
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
-          <input type='number' min='0.01' step='0.01' />
+          <input
+            type='number'
+            min='0.01'
+            step='0.01'
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type='date' min='2019-01-01' max='2022-12-31' />
+          <input
+            type='date'
+            min='2019-01-01'
+            max='2022-12-31'
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className='new-expense__actions'>
